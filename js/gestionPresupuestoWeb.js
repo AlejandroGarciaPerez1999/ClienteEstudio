9354578dcc44ca51b8ee0e11dc4e1937626fd910
Alejandro García <alexgarper7@gmail.com>
2026-05-13 16:09:18 +0200

    BorrarEtiquetasHandle a medias

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -166,6 +166,13 @@ function BorrarHandle(){
     }
 }
 
+function BorrarEtiquetasHandle(){
+    this.handleEvent = function(event){
+        this.gasto.borrarEtiquetas(this.etiqueta);
+        repintar();
+    }
+}
+
 export{
     mostrarDatoEnId,
     mostrarGastoWeb,
